153e1c0638212e5cf104c6ffdc60f618d21d5483
giovannidw <giovannidwidjosewojo@hotmail.com>
2019-02-25 04:53:11 +0100

    Idk what dis is, imma sleep now...

--- l3-vector-type.js
+++ l3-vector-type.js
@@ -1,1 +1,27 @@
- 
+ class Vec {
+
+constructor(x,y){
+ this.x = x;
+ this.y = y;
+}
+
+   plus(){
+
+   }
+   minus(){
+return Vec(x,y)
+   }
+   get length(){
+     return this.x + this.y;
+   }
+ }
+
+
+ // Your code here.
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
